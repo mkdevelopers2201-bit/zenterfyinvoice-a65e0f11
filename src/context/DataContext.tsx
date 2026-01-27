ac738217ba20dcced3f0468aa3b99bd5f4615c31
mkdevelopers2201-bit <mkdevelopers2201@gmail.com>
2026-01-27 14:47:59 +0530

    Update DataContext.tsx

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -349,7 +349,7 @@ export const useData = () => {
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xvlnlertpfdkgvitlfsc.supabase.co';
+const supabaseUrl = 'https://gkgzgermnlqtheawyhwr.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrZ3pnZXJtbmxxdGhlYXd5aHdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0NDQ3NzcsImV4cCI6MjA4NTAyMDc3N30.XMzFBWT2ropJXosmhEkEkZeZ1zSITF2I9szvS9PdeIY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
